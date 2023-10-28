e64f368e59f532c1c7d7e69f0b691f8deee5367c
Zhi Hang Yang <zhiy@ad.unc.edu>
2023-10-28 19:14:25 -0400

    dictionary progress

--- exercises/dictionary.py
+++ exercises/dictionary.py
@@ -2,10 +2,6 @@
 
 __author__ = "730713735"
 
-def main() -> None:
-    """Main function to store other functions."""
-    pass
-
 
 def invert(given_dict: dict[str,str]) -> dict[str,str]:
     """inverts the key-value pair in the dictionary."""
@@ -20,7 +16,3 @@ def invert(given_dict: dict[str,str]) -> dict[str,str]:
         dictionary[list[counter]] = i
         counter += 1
     return dictionary
-
-
-if __name__ == "__main__":
-    main()
